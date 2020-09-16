e366069fa5cc7ff72caa539aab2ddc8358015d36
Maurice Schmitz <msc@combahton.net>
2020-09-16 16:33:21 +0200

    added option to toggle flowShield v3, see cbcli antiddos --help

--- README.md
+++ README.md
@@ -1,22 +1,4 @@
 # combahton CLI
 Easy access to the combahton API via a command line tool.
 
-## Modules
-* antiddos
-* customer
-* kvm
-* login
-
-### customer
-#### invoice-all
-#### invoice-unpaid
-####  invoice
-**Required:** invoice-id (integer), outfile (string, path) \
-**Usage:** cbcli customer invoice invoice-id outfile \
-**Example:** cbcbli customer invoice 5041234 invoice.pdf \
-
-
-### login
-**Required:** email (string), apikey (string) \
-**Usage:** cbcli login email apikey \
-**Example:** cbcli login msc@combahton.net abc1234 \
+Use `$ cbcli help` to get usage information.
--- src/modules/antiddos/commands.py
+++ src/modules/antiddos/commands.py
@@ -1,4 +1,10 @@
 import click
+import json
+import ipaddress
+import sys
+from api.request import ApiRequest
+
+api = ApiRequest()
 
 @click.group()
 def antiddos():
@@ -7,9 +13,21 @@ def antiddos():
 
 
 @click.command()
-@click.argument('toggle')
-def fv3(toggle):
-    if toggle == True or toggle == "enable":
-        click.echo(toggle)
+@click.argument('ipv4')
+@click.argument('toggle', type=click.BOOL)
+def fv3(ipv4, toggle):
+    try:
+        ipaddr = str(ipaddress.ip_address(ipv4))
+        request = api.request(component = "antiddos", method = "layer4", action = "routing", routing = "l4_target", target = ('fv3' if toggle else 'fv3'), ipaddr = ipaddr)
+        response = json.loads(request.text)
+        if response and response["status"]  == 'routing_changed':
+            click.echo("OK")
+        else:
+            click.echo(response["status"])
+    except ValueError:
+        click.echo("address/netmask is invalid: %s "  % click.get_os_args()[2])
+    except:
+        click.echo('Usage: antiddos fv3 127.0.0.1 true')
+        raise
 
 antiddos.add_command(fv3)
